a8b4cd869c2d1b467d0ce52f55248b71aa2375db
joaovictorMenphins <joaomilior007@gmail.com>
2024-09-26 20:19:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,33 @@
-/* Ação de eventos sobre o botão menu da tag nav (mobile). */
-/* Parâmetros: Nome do evento, função. */
+// Ação de eventos sobre o botão menu da tag nav (mobile).
 openMenu.addEventListener('click', () => {
+    menu.style.display = "flex";
+    menu.style.right = (menu.offsetWidth * -1) + 'px';
+    
+    setTimeout(() => {
+        menu.style.opacity = '1';
+        menu.style.right = "0";
+        openMenu.style.display = 'none';
+    }, 10);
+});
 
-	/* Sobrepõe a propriedade display: none aplicada no
-	   CSS por display: flex que o torna visível. */ 
-	menu.style.display = "flex"
-
-	/* Captura o tamanho do menu nav e aplica na posição. */
-	menu.style.right = (menu.offsetWidth * -1) + 'px'
-
-	/* Após 10 milésimos de segundo, adiciona o atributo style, */
-	/* e adiciona as propriedades CSS.*/
-	setTimeout(()=> {
-		/* Faz o menu nav aparecer na velocidade em que foi
-		   determinado na propriedade transition no CSS.*/
-		menu.style.opacity = '1'
-
-		/* Move o menu nav para a posição 0 a direita. Utiliza 
-		   também a velocidade definida, na propriedade transition 
-		   no CSS para realizar o movimento mais suave.*/
-		menu.style.right = "0"
-
-		/* Oculta o botão que torna visível o elemento nav.*/
-		openMenu.style.display = 'none'
-	}, 10);
-})
-
-/* Ação de eventos sobre o botão X da tag nav (mobile). */
-/* Parâmetros: Nome do evento, função. */
+// Ação de eventos sobre o botão X da tag nav (mobile).
 closeMenu.addEventListener('click', () => {
+    menu.style.opacity = '0';
+    menu.style.right = (menu.offsetWidth * -1) + 'px';
 
-	/* Faz o menu nav desaparecer na velocidade em que foi
-	   determinado na propriedade transition no CSS. */
-	menu.style.opacity = '0'
+    setTimeout(() => {
+        menu.removeAttribute('style');
+        openMenu.removeAttribute('style');
+    }, 200);
+});
 
-	/* Captura o tamanho do menu nav e aplica na posição. */
-	menu.style.right = (menu.offsetWidth * -1) + 'px'
-
-	/* Torna visível o botão que apresenta o menu nav. */
-	/* openMenu.style.display = 'block'*/
-	
-	/* Após 200 milésimos de 1 segundo, remove o atributo style. */
-	setTimeout(()=> {
-		menu.removeAttribute('style')
-		openMenu.removeAttribute('style')
-	}, 200);
-})
+// Função para criar flocos de neve
 const snowContainer = document.getElementById('snow-container');
 
 function createSnowflake() {
     const snowflake = document.createElement('div');
     snowflake.className = 'snowflake';
-    snowflake.innerText = '❄'; // Emoji de floco de neve
+    snowflake.innerText = '❄';
 
     // Posição inicial aleatória
     snowflake.style.left = Math.random() * window.innerWidth + 'px';
